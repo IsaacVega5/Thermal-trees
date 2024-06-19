b1ffcb2b6583fcb3ed88f17032a61132e96acf9a
unknown <isaacvega361@gmail.com>
2024-06-19 09:54:53 -0400

    Se corrige el paffing de selectFolder

--- components/selectFolder.py
+++ components/selectFolder.py
@@ -6,20 +6,20 @@ from ttkbootstrap.constants import *
 class SelectFolder(ttk.Frame):
   def __init__(self, master):
     super().__init__(master)
-    self.pack()
+    self.pack(padx=0)
 
     self.master = master
     
-    self.label = ttk.Label(self, text="Seleccionar carpeta", justify="left", width=100)
-    self.label.grid(row=0, column=0, padx=0, pady=5)
+    self.label = ttk.Label(self, text="Seleccionar carpeta", justify="left")
+    self.label.grid(row=0, column=0, padx=0, pady=0, sticky=tk.W)
     
-    self.entry = ttk.Entry(self, width=100)
+    self.entry = ttk.Entry(self, width=105)
     self.entry.insert(0, self.master.path)
     self.entry.config(state="readonly")
-    self.entry.grid(row=1, column=0, padx=(0,5), pady=5)
+    self.entry.grid(row=1, column=0, padx=(0,5), pady=(0,5))
 
     self.button = ttk.Button(self, text="Seleccionar", command=self.selectFolder)
-    self.button.grid(row=1, column=2, padx=5, pady=5)
+    self.button.grid(row=1, column=2, padx=0, pady=5)
     
   def selectFolder(self):
     folder = filedialog.askdirectory()
